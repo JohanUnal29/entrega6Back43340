b7a731e9116e6c7a6a2607e18d9040f032090137
JohanUnal29 <johan.ardilah@gmail.com>
2023-06-11 18:17:21 -0500

    refac

--- src/utils/connections.js
+++ src/utils/connections.js
@@ -2,7 +2,7 @@ import { connect } from "mongoose";
 export async function connectMongo() {
   try {
     await connect(
-      "mmongodb+srv://johanardilah:Bmth2018.@dasein.vn6t9dg.mongodb.net/?retryWrites=true&w=majority"
+      "mongodb+srv://johanardilah:Bmth2018.@dasein.vn6t9dg.mongodb.net/?retryWrites=true&w=majority"
     );
     console.log("plug to mongo!");
   } catch (e) {
